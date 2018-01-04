5f4d38476d94c0f808eb5e03eb866e603b140b99
xuhaoguang <hgxu1993@163.com>
2018-01-04 14:25:03 +0800

    add a sum function in test_branch2.cpp

--- test_branch2.cpp
+++ test_branch2.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int sum(int a, int b){
+    return a + b;
+} 
+
 int main(){
     cout << "This is a test for branch2" << endl;
+    cout << "Thanks for your listerning!" << endl;
     return 0;
 } 
